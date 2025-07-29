57ed77939920abfa67800129939060898f39e8dc
mrbluguy <dennishognogiu2@gmail.com>
2025-07-29 21:40:13 +0100

    Updated Javascript file

---
+++ script.js
@@ -0,0 +1,20 @@
+function generateRandomNumber() {
+   return Math.floor(Math.random() * 10) + 1;
+
+}
+
+console.log(generateRandomNumber())
+
+function getPlayerGuess() {
+	let number = prompt("Please enter a number");
+
+    do {
+       if (1 == number <= 100) {
+	      return number;
+	   } else {
+	      number = prompt("Please enter a number (not a word)");
+	}
+   } while (number != parseInt(number));
+}
+
+console.log(getPlayerGuess())
