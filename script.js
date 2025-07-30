07006f8ac77de0a67a0a47cb78e26e6c16d331ee
mrbluguy <dennishognogiu2@gmail.com>
2025-07-30 12:00:00 +0100

    Continued working on the guessing game

--- script.js
+++ script.js
@@ -1,20 +1,84 @@
+let attempts = 0;
+const maxAttempts = 10;
+let score = 100;
+
 function generateRandomNumber() {
-   return Math.floor(Math.random() * 100) + 1;
-
+   const rannum = Math.floor(Math.random() * 100) + 1;
+   console.log(rannum);
+   return rannum;
 }
 
-console.log(generateRandomNumber())
 
-function getPlayerGuess() {
-	let number = prompt("Please enter a number");
+function getPlayerGuess() { 
+	const message = "Guess the number (1-100)";
+    let userInput = prompt(message);
 
-    do {
-       if (1 == number <= 100) {
-	      return number;
-	   } else {
-	      number = prompt("Please enter a number (not a word)");
+	
+	if (userInput === null) {
+    return null;  // pressed cancel
+  }
+
+  const number = Number(userInput);
+
+  if (isNaN(number)) {
+    alert("The input is invalid, insert a valid number");
+    return getPlayerGuess();
+  }
+  else if (number > 100) {
+    alert("Number must be between 1 and 100");
+    return getPlayerGuess();
+  }
+  else if (number < 1) {
+    alert("Number must be between 1 and 100");
+    return getPlayerGuess();
+  }
+  
+    return number; 
+};
+//console.log(getPlayerGuess())
+
+function checkGuess(userGuess, correctNumber, attempts, maxAttempts, score) {
+  if (userGuess === correctNumber) {
+    alert("Congratulations, you won");
+	return true;
+  } else if (attempts < maxAttempts) {
+	if (userGuess < correctNumber) {
+     alert("Try a higher number");
+    } else {
+     alert("Try a lower number");
 	}
-   } while (number != parseInt(number));
-}
+	
+  } return false;
+  
+};
 
-console.log(getPlayerGuess())
+function game() {
+  const correctNumber = generateRandomNumber();
+  attempts = 0;
+
+  while (attempts < maxAttempts) {
+    const userGuess = getPlayerGuess();
+	
+
+    if (userGuess === null) {
+      alert("Game cancelled.");
+      return;
+    }
+
+    attempts++;
+
+    const won = checkGuess(userGuess, correctNumber, attempts, maxAttempts, score);
+
+    if (won) {
+      alert(`You guessed it in ${attempts} attempt(s)! Here is your score: ${score}`);
+      console.log(`You guessed it in ${attempts} attempt(s)! Here is your score: ${score}`)
+      return;
+    } else {
+		score -= 10
+	}
+  }
+
+  alert(`You used all your attempts. You lost! The correct number was ${correctNumber}.`);
+};
+
+game();
